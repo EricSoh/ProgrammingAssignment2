6b373b02e2e127617e63b4d6d53a6a80abd5376e
Eric Soh HB <EricSoh@users.noreply.github.com>
2014-05-24 17:51:42 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function stores the original square matrix in "x"
+## and its inverse form in "inv". The inverse form is obtained
+## from cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL #Clear "inv" of previous data to initialize
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL #For initialization
+        }
+        get <- function() x #Store the original matrix
+        setinv <- function(solve) inv<<-solve #Store output from cacheSolve()
+        getinv <- function() inv #NULL by default, retrieve data in "inv"
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This function checks if the inverse matrix is present.
+## If not present, it will proceed to calculate the inverse
+## and print out to screen the result i.e. inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv() #Retrieve data from makeCacheMatrix()
+        if(!is.null(inv)) { #If NULL, calculate using Solve else print out "inv"
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get() #Retrieve original matrix
+        inv <- solve(data, ...) #Inverse matrix
+        x$setinv(inv) #Initialize list $setinv with data i.e. inverse matrix
+        inv #Print to screen the inverse matrix
 }
